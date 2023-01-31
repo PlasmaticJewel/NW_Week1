f2273f2a734c76278f1810e41351608f9c06fbfa
PlasmaticJewel <123777068+PlasmaticJewel@users.noreply.github.com>
2023-01-30 19:23:07 -0500

    Initial commit

---
+++ Name.java
@@ -0,0 +1,18 @@
+class NathanHomework
+{
+	public static void main(String[] args) {
+		        System.out.println("N      N        A        TTTTTTT  EEEEEEE  W                  W");
+				System.out.println("NN     N       A A          T     E         W                W");
+				System.out.println("N N    N      A   A         T     E          W      WW      W");
+				System.out.println("N   N  N     A     A        T     EEEEEE      W    W  W    W");
+				System.out.println("N    N N    AAAAAAAAA       T     E            W   W  W   W");
+				System.out.println("N     NN   A         A      T     E             W  W  W  W");
+				System.out.println("N      N  A           A     T     EEEEEEE        WW    WW");
+	}
+}
+/*Nathan Wheeler
+nrwheeler2201@student.stcc.edu 
+1/27/22
+CsC-111
+Homework 1
+This program will take print imputs to print my name as a banner*/
